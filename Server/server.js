051bf6f3a2851736649aad86729dc9bfcf430766
Jayasurya <jaya.surya@humanizetech.ai>
2025-05-24 12:41:45 +0530

    first commit

---
+++ Server/server.js
@@ -0,0 +1,20 @@
+import express from "express";
+import cors from "cors";
+import 'dotenv/config';
+import cookieParser from "cookie-parser";
+import connectDB from "./config/mongodb.js";
+
+
+const app = express();
+const port = process.env.PORT || 5000;
+connectDB();
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({credentials: true}));
+
+app.get("/", (req, res) => {
+    res.send("API Working!");
+});
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
